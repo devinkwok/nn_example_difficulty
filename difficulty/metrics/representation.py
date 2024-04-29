FIX passing intermediates_iterable to prediction_depth gives wrong results
FIX move logic for whether knn train/test parameters should be None from prediction_depth to knn_predict

difficulty/metrics/representation.py
@@ -118,7 +118,7 @@ def representation_metrics(
             model, dataloader, pd_layers, device=device, verbose=verbose, append_softmax=pd_append_softmax)
         # use true labels as consensus labels if not set
         train_labels = labels if pd_train_labels is None else pd_train_labels
-        
+
         # compute prediction depth on knn training data if test data not provided
         test_intermediates = intermediates_iterable(
             model, dataloader if pd_test_dataloader is None else pd_test_dataloader,
@@ -208,10 +208,11 @@ def knn_predict(
     if test_x is None:
         assert test_labels is None
         test_x = train_x
+        test_labels = train_labels
     else:
         assert test_labels is not None
         test_x = test_x.reshape(test_x.shape[0], -1).detach().cpu().numpy()
-        predictions = torch.tensor(knn.predict(test_x))
+    predictions = torch.tensor(knn.predict(test_x))
     match = predictions == test_labels.detach().cpu()
     return match
 
@@ -253,15 +254,6 @@ def prediction_depth(
             where matches is a torch.Tensor indicating whether the KNN prediction matches the label
             for each layer and example in the N outputs, with shape (L, N).
     """
-    # if test not set, run predict on train
-    if test_intermediates is None:
-        assert test_labels is None
-        test_intermediates = train_intermediates
-        test_labels = train_labels
-    else:
-        assert test_labels is not None
-
-    # if dict, convert to an iterator, if outputs is not None, append softmax
     if isinstance(train_intermediates, dict):
         train_intermediates = train_intermediates.values()
     if isinstance(test_intermediates, dict):
@@ -269,10 +261,20 @@ def prediction_depth(
 
     # compute knns per layer
     knn_predictions = []
-    for i, (train_x, test_x) in enumerate(zip(train_intermediates, test_intermediates)):
-        if verbose:
-            print(f"pd: fitting KNN to layer {i}")
-        knn_predictions.append(knn_predict(train_x, train_labels, test_x, test_labels, k=k))
+
+    # if test not set, run predict on train
+    if test_intermediates is None:
+        assert test_labels is None
+        for i, train_x in enumerate(train_intermediates):
+            if verbose:
+                print(f"pd: fitting KNN to layer {i}")
+            knn_predictions.append(knn_predict(train_x, train_labels, k=k))
+    else:
+        for i, (train_x, test_x) in enumerate(zip(train_intermediates, test_intermediates)):
+            if verbose:
+                print(f"pd: fitting KNN to layer {i}")
+            knn_predictions.append(knn_predict(train_x, train_labels, test_x, test_labels, k=k))
+
     matches = torch.stack(knn_predictions, dim=0)
     matches = matches.to(dtype=train_labels.dtype, device=train_labels.device)
     pd = first_unforgettable(matches)

difficulty/test/test_model.py
@@ -130,10 +130,9 @@ class TestModel(BaseTest):
 
     def test_prediction_depth(self):
         # check that outputs are correct shape and ranges
-        _, z, out, _ = self._evaluate_intermediates(self.layers)
+        _, z, _, _ = self._evaluate_intermediates(self.layers)
         train_z = [v[:self.n // 2] for v in z.values()]
         train_labels = self.data_labels[:self.n // 2]
-        train_out = out[:self.n // 2]
         pd = prediction_depth(train_z, train_labels, z, self.data_labels, k=2)
         self.assertEqual(pd.shape, (self.n,))
         self.assertTrue(torch.all(0 <= pd))
@@ -160,6 +159,20 @@ class TestModel(BaseTest):
         right_shift = [test_points + i*0.2 for i in range(3)]
         pd = prediction_depth(train_points, train_labels, right_shift, labels, k=1)
         self.tensors_equal(pd, torch.tensor([3, 2, 1, 0]))
+
+    def test_intermediates_iterable(self):
+        # check that using a generator to get intermediates for prediction_depth is the same as passing a dict of intermediates
+        # train only
+        generator = intermediates_iterable(self.model, self.dataloader, self.layers, device=self.device)
+        pd = prediction_depth(generator, self.data_labels, k=3)
+        _, z, _, _ = self._evaluate_intermediates(self.layers)
+        self.tensors_equal(pd, prediction_depth(z, self.data_labels, k=3))
+        # train and test
+        train_generator = intermediates_iterable(self.model, self.dataloader, self.layers, device=self.device)
+        test_generator = intermediates_iterable(self.model, self.dataloader, self.layers, device=self.device)
+        pd = prediction_depth(train_generator, 1 - self.data_labels, test_generator, self.data_labels, k=4)
+        _, z, _, _ = self._evaluate_intermediates(self.layers)
+        self.tensors_equal(pd, prediction_depth(z, 1 - self.data_labels, z, self.data_labels, k=4))
 
     def test_prototypes(self):
         repr_layer = 'blocks.5.relu2.out'
@@ -235,7 +248,6 @@ class TestModel(BaseTest):
 
         layers = [scores[f"pdlayer{i}"] for i in range(len(self.layers) + 1)]
         self.tensors_equal(first_unforgettable(torch.stack(layers, dim=0)), scores["pd"])
-
 
 
 if __name__ == '__main__':